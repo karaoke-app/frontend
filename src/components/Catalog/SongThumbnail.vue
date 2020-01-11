d11d8a142d57dcd8a8bff9c8eec434b50c644ee7
Mariusz Zdancewicz <mariusz@zdancewicz.it>
2020-01-11 10:41:18 +0100

    keep aspect ratio of thumbnail images

--- src/components/Catalog/SongThumbnail.vue
+++ src/components/Catalog/SongThumbnail.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="songLink" class="thumbnail-container">
-    <div class="thumbnail">
+    <div class="thumbnail-image">
       <img src="http://placehold.jp/500x350.png" />
     </div>
     <div class="thumbnail-title">{{ song.artist }} - {{ song.title }}</div>
@@ -28,43 +28,48 @@ export default {
 
 <style scoped lang="scss">
 .thumbnail-container {
+  padding: 1.5em;
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  .thumbnail {
-    margin: 1em;
-    position: relative;
-    transition: all 0.35s cubic-bezier(0.4, 0, 0.2, 1);
-
-    &::before {
-      content: "";
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      background: linear-gradient(
-        141deg,
-        #3725d4 0%,
-        #7957d5 71%,
-        #9b67df 100%
-      );
-      transform: rotate(4deg);
-      z-index: -1;
-      transition: all 0.35s cubic-bezier(0.4, 0, 0.2, 1);
-    }
-  }
-
-  &:hover .thumbnail {
+  &:hover .thumbnail-image {
     transform: scale(1.02);
 
     &::before {
       transform: rotate(7deg);
     }
   }
+}
 
-  .thumbnail-title {
-    text-align: center;
-    font-weight: bold;
+.thumbnail-image {
+  padding-bottom: 75%;
+  width: 100%;
+  margin: 1em;
+  position: relative;
+  transition: all 0.35s cubic-bezier(0.4, 0, 0.2, 1);
+
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    object-fit: cover;
   }
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(141deg, #3725d4 0%, #7957d5 71%, #9b67df 100%);
+    transform: rotate(4deg);
+    z-index: -1;
+    transition: all 0.35s cubic-bezier(0.4, 0, 0.2, 1);
+  }
+}
+
+.thumbnail-title {
+  text-align: center;
+  font-weight: bold;
 }
 </style>
